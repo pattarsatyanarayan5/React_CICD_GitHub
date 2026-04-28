d3f3a56ed778cd7cf6aac91ce0a5c05b4b8abe21
pattarsatyanarayan6 <pattarsatyanarayan6@gmail.com>
2026-04-28 08:39:22 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/React_Vite_CICD_GitHub/",
+  base:"/React_CICD_GitHub/",
   server:{
     open:true,
     port:3001
